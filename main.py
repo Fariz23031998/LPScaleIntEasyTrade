5758d186fe6f2a6dcf1add81cd71fe29b5872117
Fariz23031998 <xuramovf1888@gmail.com>
2025-02-04 22:58:30 +0500

    Bugs Fixed

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import os
 today = datetime.now().strftime("%d-%m-%Y")
 log_file_name = f'{today}-log.txt'
 
-
 def get_date():
     now = datetime.now()
     return now.strftime("%m/%d/%Y %H:%M:%S")
@@ -25,6 +24,26 @@ def write_log_file(text, line):
         file.write(f"{text} (Line: {line}, {get_date()})\n")
 
 
+def get_unit_type_from_id(unit_id):
+    for key, value in sale_units.items():
+        if value["easy_trade_id"] == unit_id:
+            return value
+
+
+def write_to_csv(data, filename="output.csv"):
+    with open(filename, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+        writer.writerows(data)
+
+
+def create_query_arg(units_config):
+    et_units_id = tuple(value["easy_trade_id"] for value in units_config.values() if value["easy_trade_id"])
+    if len(et_units_id) == 1:
+        return f"AND G.gd_unit = {et_units_id[0]}"
+    else:
+        return f"AND G.gd_unit IN {et_units_id}"
+
+
 with open("config.txt", encoding='utf-8') as config_file:
     config = eval(config_file.read())
 
@@ -35,35 +54,52 @@ user = config["user"]
 password = config["password"]
 divider = config["divider"]
 check_time = config["check_time"]
-weight_prefix = config["weight_prefix"]
-piece_prefix = config["piece_prefix"]
-barcode_type = config["barcode_type"]
-piece_label_format = config["piece_label_format"]
-weight_label_format = config["weight_label_format"]
 only_selected_group = config["only_selected_group"]
-
 sale_units = config['sale_units']
-sale_units_tuple = tuple(filter(None, sale_units.values()))
-
-lp_units = config['weight_units_lp']
 
 
-def get_unit_type_from_id(unit_id):
-    unit_key = None
-    for key, value in sale_units.items():
-        if value == unit_id:
-            unit_key = key
-            break
-    if unit_key == "piece":
-        return 2, piece_prefix, piece_label_format
-    elif unit_key:
-        return lp_units.get(unit_key), weight_prefix, weight_label_format
+units_arg = create_query_arg(sale_units)
+
+query_fetch_items_with_group = f"""
+    SELECT  
+        G.gd_code, 
+        G.gd_name, 
+        G.gd_unit,  
+        P.prc_value
+    FROM easytrade_db.dir_goods G
+        LEFT JOIN easytrade_db.dir_prices P ON G.gd_id = P.prc_good
+        LEFT JOIN easytrade_db.dir_groups GR ON G.gd_group = GR.grp_id
+    WHERE 
+        G.gd_deleted_mark = 0 
+        AND G.gd_deleted = 0 
+        AND P.prc_type = %s
+        AND P.prc_value > 0
+        AND GR.grp_name LIKE '%#456%'
+        {units_arg}
+    ORDER BY G.gd_code
+"""
+
+query_fetch_items = f"""
+    SELECT  
+        G.gd_code, 
+        G.gd_name, 
+        G.gd_unit,  
+        P.prc_value
+    FROM easytrade_db.dir_goods G
+        LEFT JOIN easytrade_db.dir_prices P ON G.gd_id = P.prc_good
+    WHERE 
+        G.gd_deleted_mark = 0 
+        AND G.gd_deleted = 0 
+        AND P.prc_type = %s
+        AND P.prc_value > 0
+        {units_arg}
+    ORDER BY G.gd_code
+"""
 
 
-def write_to_csv(data, filename="output.csv"):
-    with open(filename, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f, delimiter=';', quoting=csv.QUOTE_MINIMAL)
-        writer.writerows(data)
+if not os.path.exists(log_file_name):
+    with open(log_file_name, 'w', encoding='utf-8') as file:
+        file.write(f"File created at {get_date()}\n")
 
 
 class UpdateData:
@@ -76,14 +112,6 @@ class UpdateData:
         self.is_mysql_connected = False
         self.connect_mysql()
 
-        if not os.path.exists(log_file_name):
-            with open(log_file_name, 'w', encoding='utf-8') as file:
-                file.write(f"File created at {self.get_date()}\n")
-
-    def get_date(self):
-        now = datetime.now()
-        return now.strftime("%m/%d/%Y %H:%M:%S")
-
     def connect_mysql(self):
         try:
             self.mysql_conn = mysql.connector.connect(
@@ -94,7 +122,7 @@ class UpdateData:
             )
         except Error as e:
             print(f"Can't connect to the MySQL. {e}. Line: {get_line_number()}")
-            write_log_file(f"Can't connect to the MySQL. {e} {self.get_date()}", get_line_number())
+            write_log_file(f"Can't connect to the MySQL. {e} {get_date()}", get_line_number())
             self.is_mysql_connected = False
             return False
         else:
@@ -144,54 +172,32 @@ class UpdateData:
             mysql_cursor.execute("RESET QUERY CACHE")
 
             if only_selected_group:
-                mysql_query = """
-                    SELECT  
-                        G.gd_code, 
-                        G.gd_name, 
-                        G.gd_unit,  
-                        P.prc_value
-                    FROM easytrade_db.dir_goods G
-                        LEFT JOIN easytrade_db.dir_prices P ON G.gd_id = P.prc_good
-                        LEFT JOIN easytrade_db.dir_groups GR ON G.gd_group = GR.grp_id
-                    WHERE 
-                        G.gd_deleted_mark = 0 
-                        AND G.gd_deleted = 0 
-                        AND P.prc_type = %s
-                        AND P.prc_value > 0
-                        AND GR.grp_name LIKE '%#456%'
-                    ORDER BY G.gd_code
-                """
+                mysql_cursor.execute(query_fetch_items_with_group, (self.price_type_id,))
+                items = mysql_cursor.fetchall()
             else:
-                mysql_query = """
-                    SELECT  
-                        G.gd_code, 
-                        G.gd_name, 
-                        G.gd_unit,  
-                        P.prc_value
-                    FROM easytrade_db.dir_goods G
-                        LEFT JOIN easytrade_db.dir_prices P ON G.gd_id = P.prc_good
-                    WHERE 
-                        G.gd_deleted_mark = 0 
-                        AND G.gd_deleted = 0 
-                        AND P.prc_type = %s
-                        AND P.prc_value > 0
-                    ORDER BY G.gd_code
-                """
+                mysql_cursor.execute(query_fetch_items, (self.price_type_id,))
+                items = mysql_cursor.fetchall()
 
-            mysql_cursor.execute(mysql_query, (self.price_type_id, ))
-            items = mysql_cursor.fetchall()
+        except Error as e:
+            print(f"Line: {get_line_number()}, Can't connect to the MySQL. {e} {get_date()}")
+            write_log_file(f"Can't connect to the MySQL. {e}", get_line_number())
+            self.connect_mysql()
+
+        else:
             items_list = []
             for item in items:
-                unit_info_tuple = get_unit_type_from_id(item[2])
-                if unit_info_tuple:
-                    unit_id = unit_info_tuple[0]
-                    unit_prefix = unit_info_tuple[1]
-                    unit_label_format = unit_info_tuple[2]
+                unit_info_dict = get_unit_type_from_id(item[2])
+                if unit_info_dict:
+                    unit_id = unit_info_dict["lp_scale_id"]
+                    unit_prefix = unit_info_dict["prefix"]
+                    unit_label_format = unit_info_dict["label_format"]
+                    unit_sale_type = unit_info_dict["sale_type"]
+                    unit_barcode_type = unit_info_dict["barcode_type"]
                     item_list = [
                         item[0],
                         item[1],
                         f'{unit_prefix}{item[0]}',
-                        barcode_type,
+                        unit_barcode_type,
                         unit_label_format,
                         item[3] / divider,
                         unit_id,
@@ -200,7 +206,7 @@ class UpdateData:
                         0,
                         0,
                         0,
-                        0,
+                        unit_sale_type,
                         0,
                         0,
                         0,
@@ -209,10 +215,6 @@ class UpdateData:
                     items_list.append(item_list)
 
             write_to_csv(items_list, filename='products.csv')
-        except Error as e:
-            print(f"Line: {get_line_number()}, Can't connect to the MySQL. {e} {get_date()}")
-            write_log_file(f"Can't connect to the MySQL. {e}", get_line_number())
-            self.connect_mysql()
 
 
 update_data = UpdateData()
